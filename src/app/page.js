334f5c4b17bc2cd63e0db0c3254b1532890f6929
Rawan <r.k096@hotmail.com>
2024-03-23 14:32:08 +0300

    feat: add Hero page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 
-import Card from "./components/card";
+import HomeSlider from "./components/home-slider";
 import GlobalApi from "./services/globalApi";
-import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 import Hero from "./components/hero";
 
 export default function Home() {
   const [movieList, setMovieList] = useState([]);
-  const elementRef = useRef(null);
+ 
   useEffect(() => {
     getPopMovies();
   }, []);
@@ -19,46 +18,13 @@ export default function Home() {
   })
   };
 
-  const slideRight = (element) => {
-    element.scrollLeft += 500;
-  };
-  const slideLeft = (element) => {
-    element.scrollLeft -= 500;
-  };
   return (
     <>
 
       <Hero/>
-   
+      <HomeSlider movieList={movieList} title={"Latest Movies"} background={"gradient-blue"}/>
       {/* <div>test</div> */}
-      <div className="la">
-      <h1 className="pl-20 pt-12 font-body text-[96px]">Latest Movies</h1>
-      <div className="relative">
-        <IoChevronBackOutline
-          onClick={() => slideLeft(elementRef.current)}
-          className="text-[50px] text-white
-           p-2 z-10 cursor-pointer 
-            hidden md:block absolute top-1/2"
-        />
-        <div
-         ref={elementRef}
-          className=" la flex overflow-x-auto gap-4
-         scrollbar-none scroll-smooth pt-4 px-20 pb-4"
-        >
-          {movieList.map((movie) => (
-            <div>
-              <Card key={movie.id} movie={movie}/>
-            </div>
-          ))}
-        </div>
-        <IoChevronForwardOutline
-          onClick={() => slideRight(elementRef.current)}
-          className="text-[50px] text-white hidden md:block
-           p-2 cursor-pointer z-10 
-            absolute right-0 top-1/2"
-        />
-      </div>
-      </div>
+     
     </>
   );
 }
