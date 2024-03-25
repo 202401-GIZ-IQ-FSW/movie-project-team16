44ea260c2887f8938917a231441cfe0bcd063739
Sarah Azad <aras.azad3@gmail.com>
2024-03-25 23:43:42 +0300

    style: responsive pages

--- src/app/genere/[type]/page.js
+++ src/app/genere/[type]/page.js
@@ -22,16 +22,16 @@ export default function Movies({ params }) {
   return (
     <div className="bg-black">
       <div className="actor-hero  flex items-center h-[400px] px-12 py-6">
-        <h1 className="font-body  text-white text-[62px] font-light py-6">
+        <h1 className="font-body  text-white text-[38px] lg:text-[62px] font-light py-6">
           It's time for some
           <p
-            className={`${params.type} font-bod text-[110px] text-cente font-bold p-0 opacity-95`}
+            className={`${params.type} font-bod text-[56px] lg:text-[110px] text-cente font-bold p-0 opacity-95`}
           >
             {params.type}!
           </p>
         </h1>
       </div>
-      <div className="flex w-screen h-fit flex-wrap p-10 pb-20 gap-6">
+      <div className="flex w-screen h-fit flex-wrap p-10 justify-center pb-20 gap-6">
         {movieList.map((movie) => (
           <Card movie={movie} key={movie.id} />
         ))}
