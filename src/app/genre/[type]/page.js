4389a8fbfcf4e8df5f0efc5d8ceff11c185cf014
Rawan <r.k096@hotmail.com>
2024-04-03 15:19:20 +0300

    bug fixes

--- src/app/genre/[type]/page.js
+++ src/app/genre/[type]/page.js
@@ -21,17 +21,17 @@ export default function Movies({ params }) {
   };
   return (
     <div className="bg-black">
-      <div className="actor-hero  flex items-center h-[400px] px-12 py-6">
-        <h1 className="font-body  text-white text-[38px] lg:text-[62px] font-light py-6">
+      <div className="actor-hero  flex items-center h-[400px] px-12 ">
+        <h1 className="font-body  text-white text-[38px] lg:text-[62px] font-light ">
           It&apos;s time for some
           <p
-            className={`${genreName} font-bod text-[110px] text-cente font-bold p-0 opacity-95`}
+            className={`${genreName} font-bod text-[62px]  md:text-[110px] text-cente font-bold p-0 opacity-95`}
           >
             {genreName}!
           </p>
         </h1>
       </div>
-      <div className="flex w-screen h-fit flex-wrap p-10 justify-center pb-20 gap-6">
+      <div className="flex w-screen h-fit flex-wrap justify-center pb-20 gap-6">
         {movieList.map((movie) => (
           <Card movie={movie} key={movie.id} />
         ))}
